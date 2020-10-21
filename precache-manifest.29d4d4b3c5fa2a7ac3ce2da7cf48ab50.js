96eb291614508a21729abbb21c4cde7bbde7ae23
Jaret Hill <jaret.hill@tuta.io>
2020-10-20 19:37:08 -0500

    Updates

---
+++ precache-manifest.29d4d4b3c5fa2a7ac3ce2da7cf48ab50.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df61c127f0079c8e5fddce845fa4ad0",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "25b42ebf120d36a4aae0",
+    "url": "/Memory-Game/static/css/main.28ae6c51.chunk.css"
+  },
+  {
+    "revision": "73de197224fa5d6eaf59",
+    "url": "/Memory-Game/static/js/2.41f0156a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Memory-Game/static/js/2.41f0156a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b42ebf120d36a4aae0",
+    "url": "/Memory-Game/static/js/main.e37684cf.chunk.js"
+  },
+  {
+    "revision": "a620562155869f915d9a",
+    "url": "/Memory-Game/static/js/runtime-main.0da77a42.js"
+  },
+  {
+    "revision": "fa918dd89f2ac1b458fde6d30bb7e8b2",
+    "url": "/Memory-Game/static/media/celeste.fa918dd8.jpg"
+  },
+  {
+    "revision": "64a6162a4fa265f88c069787963358e3",
+    "url": "/Memory-Game/static/media/etna.64a6162a.jpg"
+  },
+  {
+    "revision": "ecf18b12a971f629fd3268379bab9768",
+    "url": "/Memory-Game/static/media/gwyndolin.ecf18b12.jpg"
+  },
+  {
+    "revision": "fea3b322358514f2af3ae2c40bf5d33d",
+    "url": "/Memory-Game/static/media/lifestealer.fea3b322.jpg"
+  },
+  {
+    "revision": "18b5c4251ce5be0c777c4661b8ca9efd",
+    "url": "/Memory-Game/static/media/mega-man.18b5c425.png"
+  },
+  {
+    "revision": "4b80e470e1a5b7824a2a6a570dcab019",
+    "url": "/Memory-Game/static/media/miriam.4b80e470.jpg"
+  },
+  {
+    "revision": "3a76b04b79aae5d459ba690bc5a5ddba",
+    "url": "/Memory-Game/static/media/plague-knight.3a76b04b.webp"
+  },
+  {
+    "revision": "d3a079682081ddb84324c22ba54852d3",
+    "url": "/Memory-Game/static/media/raziel.d3a07968.jpg"
+  },
+  {
+    "revision": "bee2197342840cfc6de1e3bbe2927368",
+    "url": "/Memory-Game/static/media/reptile.bee21973.jpg"
+  },
+  {
+    "revision": "62d445e87d3be97070395ca584f7f494",
+    "url": "/Memory-Game/static/media/skull-kid.62d445e8.png"
+  },
+  {
+    "revision": "0bc6c558f6c5b4206a0f338a0464ecef",
+    "url": "/Memory-Game/static/media/trevor-philips.0bc6c558.jpg"
+  },
+  {
+    "revision": "2278b80c956d5dbdd649ce86f71df472",
+    "url": "/Memory-Game/static/media/tyrael.2278b80c.jpg"
+  }
+]);
